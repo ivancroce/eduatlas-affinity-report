de269924eaa341ce35721686c9755d4530385a19
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-09-07 22:07:22 +0100

    add CORS configuration

--- backend/src/main/java/com/ivancroce/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ivancroce/backend/config/SecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +36,20 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests((authorized -> authorized
                 .requestMatchers("/**").permitAll()));
 
+        httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
