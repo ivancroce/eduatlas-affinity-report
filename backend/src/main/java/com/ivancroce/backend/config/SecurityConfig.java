e63b653261657fafd6212d6f153fd3ababc5e273
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-11-27 17:41:52 +0000

    Add netlify url to CORS

--- backend/src/main/java/com/ivancroce/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ivancroce/backend/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://eduatlas-affinity-report.netlify.app"));
 
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
